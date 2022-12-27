3a0baf5c17da90fb2262d65d8ab02c581aee0926
MOHAMMED GHOUSE <121455364+m-ghouse123@users.noreply.github.com>
2022-12-27 15:04:02 +0530

    Update streamlit_app.py
    used text, header and text functions

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('test practince github')
+streamlit.header('test practince github header')
+streamlit.text('test practince github breakfast')
+streamlit.text('test practince github lunch')
+streamlit.text('test practince github dinner')
