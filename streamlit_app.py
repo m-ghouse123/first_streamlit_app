c2b1339c6bbc12a100c6839d5657f4c0966d6d3e
MOHAMMED GHOUSE <121455364+m-ghouse123@users.noreply.github.com>
2023-01-04 19:28:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 # Let's put a pick list here so they can pick the fruit they want to include
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Apple','Banana'])
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Apple','Banana','Grapefruit'])
 streamlit.dataframe(my_fruit_list)
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
