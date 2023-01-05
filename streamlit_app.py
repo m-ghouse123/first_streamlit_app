7d4b351454d4c7bcce6520313b07c73b2055d089
MOHAMMED GHOUSE <121455364+m-ghouse123@users.noreply.github.com>
2023-01-05 19:20:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ fruityvice_normalized = pa.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.write('The user entered ', fruit_choice)
 streamlit.dataframe(fruityvice_normalized)
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pa.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 # streamlit.dataframe(my_fruit_list)
 # Let's put a pick list here so they can pick the fruit they want to include
 my_fruit_list = my_fruit_list.set_index('Fruit')
